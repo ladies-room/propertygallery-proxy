a16754acfb955f05c4c210c90cd3d3108fedc39a
Susan Tan <suetan93@gmail.com>
2020-08-18 00:06:38 -0700

    http proxy

--- proxy/index.js
+++ proxy/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+const serverOne = 'http://localhost:3003';
+
 
 const PORT = 3005;
+
 const dist = path.join(__dirname, '../public');
 
 app.use(express.static(dist));
 app.use(express.json());
+
+app.all('/property/26', function(req, res) {
+  console.log('Redirecting to ServerOne');
+  apiProxy.web(req, res, {target: serverOne});
+});
+
 
 app.listen(PORT, (err) => {
   if (err) {

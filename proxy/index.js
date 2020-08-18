fdc6f54852fe24904ccba0dfddfcb8c9c7ebd1cf
Susan Tan <suetan93@gmail.com>
2020-08-17 17:39:07 -0700

    Additional updates, moved folders

--- proxy/index.js
+++ proxy/index.js
@@ -1,2 +1,17 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+
+const PORT = 3005;
+const dist = path.join(__dirname, '../public');
+
+app.use(express.static(dist));
+app.use(express.json());
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server running on: ', PORT);
+  }
+});

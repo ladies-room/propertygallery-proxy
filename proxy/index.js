5022bc575e19a4b045c26964c4a4931c329e816c
Susan Tan <suetan93@gmail.com>
2020-08-19 09:47:27 -0700

    Proxy rendering all components

--- proxy/index.js
+++ proxy/index.js
@@ -2,16 +2,42 @@ const express = require('express');
 const app = express();
 const axios = require ('axios');
 const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const PORT = 3005;
 
 const dist = path.join(__dirname, 'public');
-console.log(dist)
 
 app.use(express.static(dist));
 
-app.use('/property/:id', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+//GALLERY
+app.get('/property/:id', (req, res) => {
+  axios.get('http://localhost:3003' + req.url)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(console.log)
+});
+
+//CALENDAR
+app.get("/listing", (req, res) => {
+  var getListingAddress = 'http://localhost:2046/listing'
+  axios.get(`${getListingAddress}/`)
+    .then(response => {
+      res.send(response.data)
+    })
+});
+
+// MOREPLACES
+app.all("/stays/*", (req, res) => {
+  axios({
+    method: req.method,
+    url: "http://localhost:3001" + req.originalUrl,
+    headers: req.headers,
+    data: req.data
+  }).then((response) => {
+    res.send(response.data);
+  }).catch((err) => console.log(err));
+});
 
 app.listen(3005, (err) => {
   if (err) {

99979adefea19e3f45e162a5396a242a87d03302
Susan Tan <suetan93@gmail.com>
2020-08-23 20:15:18 -0700

    updated proxy

--- proxy/index.js
+++ proxy/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const axios = require ('axios');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = 3005;
 
 const dist = path.join(__dirname, 'public');
 
+app.use(express.json());
 app.use(express.static(dist));
 
 //GALLERY
@@ -28,15 +30,34 @@ app.get("/listing", (req, res) => {
 });
 
 // MOREPLACES
-app.all("/stays/*", (req, res) => {
-  axios({
-    method: req.method,
-    url: "http://18.204.156.27:3001" + req.originalUrl,
-    headers: req.headers,
-    data: req.data
-  }).then((response) => {
-    res.send(response.data);
-  }).catch((err) => console.log(err));
+app.get('/stays/*', (req, res) => {
+  axios.get("http://18.204.156.27:3001"+req.originalUrl)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post('/stays/*', (req, res) => {
+  axios.post("http://18.204.156.27:3001"+req.originalUrl, req.body)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.put('/stays/*', (req, res) => {
+  axios.put("http://18.204.156.27:3001"+req.originalUrl, req.body)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 app.listen(3005, (err) => {
